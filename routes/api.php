2d005a83e0094f7cdcb11d9e1529f6a8e345cd23
Wernich Wilsenach <melktert@gmail.com>
2022-03-24 19:12:41 +0200

    Add login
    Create AuthController with login route.

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ use Illuminate\Support\Facades\Route;
 // Route::delete('users/{id}', 'UserController@destroy');
 
 Route::apiResource('users', 'UserController');
+Route::post('login', 'AuthController@login');
