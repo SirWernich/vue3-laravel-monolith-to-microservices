af63f1f29fa5b1125f63d44496eec0c4ea7f7533
Wernich Wilsenach <melktert@gmail.com>
2022-03-25 19:39:16 +0200

    Add user registering

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserCreateRequest;
 use App\Http\Requests\UserUpdateRequest;
 use App\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ use Illuminate\Support\Facades\Route;
 // Route::delete('users/{id}', 'UserController@destroy');
 
 Route::post('login', 'AuthController@login');
+Route::post('register', 'AuthController@register');
 
 Route::group(['middleware' => 'auth:api'], function() {
     Route::apiResource('users', 'UserController');
