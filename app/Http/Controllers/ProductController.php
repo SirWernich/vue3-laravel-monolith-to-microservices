86909a1834626642be42069bc8e84d5450c89d75
Wernich Wilsenach <melktert@gmail.com>
2022-03-27 21:57:22 +0200

    Add image upload route

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductCreateRequest;
 use App\Http\Resources\ProductResource;
 use App\Product;
 use Illuminate\Http\Request;
@@ -22,24 +23,20 @@ class ProductController extends Controller
         return new ProductResource(Product::find($id));
     }
 
-    public function store(Request $request)
+    public function store(ProductCreateRequest $request)
     {
-        $file = $request->file('image');
-        $name = Str::random(20);
-        $url = \Storage::putFileAs('images', $file, $name . '.' . $file->extension());
-
-        $product = Product::create([
-            'title' => $request->input('title'),
-            'description' => $request->input('description'),
-            'price' => $request->input('price'),
-            'image' => env('APP_URL') . '/' . $url
-        ]);
+        $product = Product::create($request->only('title', 'description', 'price', 'image'));
 
         return response($product, Response::HTTP_CREATED);
     }
 
     public function update(Request $request, $id)
     {
+        $product = Product::find($id);
+
+        $product->update($request->only('title', 'description', 'price', 'image'));
+
+        return response($product, Response::HTTP_ACCEPTED);
     }
 
     public function destroy($id)
--- routes/api.php
+++ routes/api.php
@@ -26,8 +26,11 @@ Route::post('register', 'AuthController@register');
 
 Route::group(['middleware' => 'auth:api'], function() {
     Route::get('user', 'UserController@user');
+
     Route::put('users/info', 'UserController@updateInfo');
     Route::put('users/password', 'UserController@updatePassword');
+
+    Route::post('upload', 'ImageController@upload');
 
     Route::apiResource('users', 'UserController');
     Route::apiResource('roles', 'RoleController');
