61100d5bea61364029e0425542277c6fcbcffdfb
Wernich Wilsenach <melktert@gmail.com>
2022-03-29 06:49:20 +0200

    Add orders and orderItems migrations, models, seeders

---
+++ app/OrderItem.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\OrderItem
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderItem newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderItem newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderItem query()
+ * @mixin \Eloquent
+ */
+class OrderItem extends Model
+{
+    //
+}
